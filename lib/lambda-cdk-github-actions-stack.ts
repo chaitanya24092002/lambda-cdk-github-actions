b070f4b9289685a86a9e2b28ed1fada4c04d5ad1
Chaitanya Suryawanshi <chaitanya.suryawanshi@splus.local>
2026-07-15 15:29:16 +0530

    Update CDK project

--- lib/lambda-cdk-github-actions-stack.ts
+++ lib/lambda-cdk-github-actions-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class LambdaCdkGithubActionsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCdkGithubActionsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, 'HelloLambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      functionName: 'HelloLambdaCDK',
+      description: 'Hello from AWS Lambda deployed using CDK and GitHub Actions',
+    });
   }
 }
